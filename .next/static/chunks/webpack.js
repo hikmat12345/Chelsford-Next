2acc82331786b0b5503a87510076f930ae9590c5
Ali Naqi Hassan <121166033+Ali-Selteq@users.noreply.github.com>
2023-01-02 15:46:28 +0500

    added git ignor

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "52744e2b8ce3d8ba"; }
+/******/ 		__webpack_require__.h = function() { return "a46d517ea26af300"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
